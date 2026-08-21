8d5ed06bf3ebb20dce908893cf465122989175c0
danishkhan136 <21sw136@students.muet.edu.pk>
2026-08-21 10:22:22 +0500

    commit 22 chapter 12 advance python

---
+++ chapters practice sets/chapter12/problem1.py
@@ -0,0 +1,26 @@
+try:
+    
+    with open("chapters practice sets/chapter12/1.txt","r") as f:
+        print(f.read())
+
+except Exception as e:
+    print(e);
+
+try:
+    
+    with open("chapters practice sets/chapter12/2.txt","r") as f:
+        print(f.read())
+
+except Exception as e:
+    print(e);
+
+try:
+    
+    with open("chapters practice sets/chapter12/3.txt","r") as f:
+        print(f.read())
+
+except Exception as e:
+    print(e);
+
+
+print("Thnaks")
